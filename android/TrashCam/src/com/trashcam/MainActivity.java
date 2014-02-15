3a366562f361246d656fbeaa59fb2e17744e850f
Jun Kui <junkuits@gmail.com>
2014-02-15 13:11:41 +0800

    viewpager

--- android/TrashCam/src/com/trashcam/MainActivity.java
+++ android/TrashCam/src/com/trashcam/MainActivity.java
@@ -1,23 +1,22 @@
 package com.trashcam;
 
-import android.os.Bundle;
 import android.app.Activity;
-import android.view.Menu;
+import android.os.Bundle;
+import android.support.v4.view.ViewPager;
 
 public class MainActivity extends Activity {
 
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
-    }
+	TrashAdapter mAdapter;
 
+	ViewPager mPager;
 
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.main, menu);
-        return true;
-    }
-    
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_main);
+		mAdapter = new TrashAdapter(getFragmentManager());
+		mPager = (ViewPager) findViewById(R.id.pager);
+		mPager.setAdapter(mAdapter);
+	}
+
 }
